7297e992e17f4634f5869db2e8c13b1ffdcca930
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2018-05-08 02:56:53 +0900

    ソースマップ対応

--- karma.conf.js
+++ karma.conf.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = function(config) {
   config.set({
     basePath: '',
@@ -18,10 +20,10 @@ module.exports = function(config) {
 
     browsers: ['Firefox'],
 
-    plugins: ['karma-firefox-launcher', 'karma-jasmine', 'karma-webpack'],
+    plugins: ['karma-firefox-launcher', 'karma-jasmine', 'karma-webpack', 'karma-sourcemap-loader'],
 
     preprocessors: {
-      'app/test.ts': ['webpack']
+      'app/test.ts': ['webpack', 'sourcemap']
     },
 
     webpack: {
@@ -71,9 +73,22 @@ module.exports = function(config) {
       },
       performance: {
         hints: false
-      }
+      },
+      devtool: 'inline-source-map',
+      plugins: [
+        new webpack.SourceMapDevToolPlugin({
+          filename: null, // if no value is provided the sourcemap is inlined
+          test: /\.(ts|js)($|\?)/i // process .js and .ts files only
+        })
+      ]
     },
 
-    color: true
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+
+    color: true,
+
+    reporters: ['progress']
   });
 };
